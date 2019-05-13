a42da7896b499e32335ceb85896d328ef67fa089
yushiwang233 <yushi-wang@live.ca>
2019-05-12 22:19:36 -0400

    Debug

--- bin/PrisonersDilemma.java
+++ bin/PrisonersDilemma.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class PrisonersDilemma
 {
-	// Formatter
+    // Formatter
     public static final DecimalFormat df = new DecimalFormat("+#.##%;-#.##%");
 
     // Declare parameters and constants
@@ -15,18 +15,18 @@ public class PrisonersDilemma
     public static final int chromosomes = 71;          // Chromosome length (number of possible game histories)
     public static final int generations = 10000;         // Number of generations
     public static final int elite = (int)(0.05 * population); // Percentage of solutions to clone
-	public static final int rounds = population-1;      //number of rounds that each player plays in a generation
-	public static final int movesTotal = 100;           //each round contains 100 moves (each move == cooperate/defect)
+    public static final int rounds = population-1;      //number of rounds that each player plays in a generation
+    public static final int movesTotal = 100;           //each round contains 100 moves (each move == cooperate/defect)
 
     // Allocate memory to store solutions + associated score and fitness
     public static int[][] solutions = new int[population][chromosomes]; // 2D array storing a chromosome for each player of the "population"
-	public static double[] score = new int[population];                 // 1D array storing the score of each player in a generation
+    public static int[] score = new int[population];                 // 1D array storing the score of each player in a generation
     public static double[] fitness = new double[population];            // 1D array storing the "fitness" of each player in the "population"
-	
-	//start main method
-	public static void main(String[] args)
+
+    //start main method
+    public static void main(String[] args)
     {
-		// Open output files
+        // Open output files
         PrintWriter outputFile = null;
         try
         {
@@ -71,26 +71,26 @@ public class PrisonersDilemma
                 else
                     solutions[i][j] = 1;
             }
-			score[i] = 0;
+            score[i] = 0;
             fitness[i] = 0.;
         }
 
         // Output
         System.out.println("Generation 0");
         for (int i = 0; i < population; i++)
-		{
+        {
             printChromosome(solutions, i);
         }
-		
+
         // Evaluate initial fitness
         fitness();
-		
+
         // Keep track of best solutions in a given generation
         int[] best = new int[elite];
         for (int i = 0; i < elite; i++)
-		{
+        {
             best[i] = 0;
-		}
+        }
 
         // Start generation loop
         for (int i = 1; i < generations; i++)
@@ -151,7 +151,7 @@ public class PrisonersDilemma
                     }
 
                 }
-				
+
                 else
                 {
                     for (int j = 0 ; j<chromosomes; j++)
@@ -160,7 +160,7 @@ public class PrisonersDilemma
                         tmp[count+1][j] = solutions[parents[1]][j];
                     }
                 }
-				
+
 
                 // Mutation
                 for (int j = 0; j<chromosomes; j++)
@@ -198,12 +198,12 @@ public class PrisonersDilemma
 
             // Calculate average fitness of population and output
             double sumScore = 0.0;
-			double maxScoreGen = population * rounds * movesTotal * 3.0 //maximum collective score in a generation
-			
-			//***note*** (max collective score) is not equal to (max individual score)*population
-			//because everyone cannot get 5pts at each move. If a player gets 5, his opponent has 0.
-			//the max collective score would result in each person obtaining 3 at each move (cooperation+cooperation)
-			
+            double maxScoreGen = population * rounds * movesTotal * 3.0; //maximum collective score in a generation
+
+            //***note*** (max collective score) is not equal to (max individual score)*population
+            //because everyone cannot get 5pts at each move. If a player gets 5, his opponent has 0.
+            //the max collective score would result in each person obtaining 3 at each move (cooperation+cooperation)
+
             for (int j = 0; j < population; j++){
                 sumScore += score[j];
             }
@@ -265,9 +265,9 @@ public class PrisonersDilemma
                 currentBestAsArray.flush();
             }
         }
-		
-		for (int i = 0; i < population; i++)
-		{
+
+        for (int i = 0; i < population; i++)
+        {
             printChromosome(solutions, i);
         }
 
@@ -277,70 +277,70 @@ public class PrisonersDilemma
         outputFile.close();
         currentBestAsList.close();
         currentBestAsArray.close();
-	}
-	
-	public static void fitness()
+    }
+
+    public static void fitness()
     {
-		//Evaluate score
-		score();
-		
-		double maxScorePlayer = rounds * movesTotal * 5.0; //maximum score that each player can get in a generation (max 5 pts at every move)
-		
-		for (int i = 0; i < population; i++)
+        //Evaluate score
+        score();
+
+        double maxScorePlayer = rounds * movesTotal * 5.0; //maximum score that each player can get in a generation (max 5 pts at every move)
+
+        for (int i = 0; i < population; i++)
         {
-			fitness[i] = score[i]/maxScorePlayer;
+            fitness[i] = score[i]/maxScorePlayer;
         }
-		
-	}
-	
-	public static void score()
-	{
-		//re-initialize score of all population to 0
-		
-		for (int i = 0; i < population; i++)
+
+    }
+
+    public static void score()
+    {
+        //re-initialize score of all population to 0
+
+        for (int i = 0; i < population; i++)
         {
-			score[i] = 0;
+            score[i] = 0;
         }
-		
-		//each player plays a round with every other player in the population
-		// -1 stands for no history, 0 for defect, 1 for cooperate
-		for (int player1 = 0; player1<population; player1++)
-		{		
-			for (int player2 = player1+1; player2<population; player2++) 
-			{
-				int[] history1 = new int[6]; //history for player1
-				int[] history2 = new int[6]; //history for player2
+
+        //each player plays a round with every other player in the population
+        // -1 stands for no history, 0 for defect, 1 for cooperate
+        for (int player1 = 0; player1<population; player1++)
+        {
+            for (int player2 = player1+1; player2<population; player2++)
+            {
+                int[] history1 = new int[6]; //history for player1
+                int[] history2 = new int[6]; //history for player2
 				/*
 				The 6 integers in the array "history" represent (in order):
 				player's first move, opponent's second move
 				player's second move, opponent's second move
 				player's third move, opponent's third move
 				*/
-				
-				//at the beginning of a round, initialize all history to no history(-1)
-				for (int i=0; i<6; i++)
-				{
-					history1[i] = -1; //history for player1
-					history2[i] = -1; //history for player2
-				} 
-				
-				for (int moves = 0; moves < movesTotal; moves++) 
-				{
-					int genePlayer1 = situation(history1);
-					int genePlayer2 = situation(history2);
-					int movePlayer1 = solutions[player1][genePlayer1];
-					int movePlayer2 = solutions[player2][genePlayer2];
-					
-					//Update history after player1 and player2 both make moves
-					for (i=0; i<4; i++)
-					{
-						history1[i] = history1[i+2];
-						history2[i] = history2[i+2];
-					}
-					history1[4] = movePlayer1;
-					history1[5] = movePlayer2;
-					history2[4] = movePlayer2;
-					history2[5] = movePlayer1;
+
+                //at the beginning of a round, initialize all history to no history(-1)
+                for (int i=0; i<6; i++)
+                {
+                    history1[i] = -1; //history for player1
+                    history2[i] = -1; //history for player2
+                }
+
+                for (int moves = 0; moves < movesTotal; moves++)
+                {
+                    int genePlayer1 = situation(history1);
+                    int genePlayer2 = situation(history2);
+                    int movePlayer1 = solutions[player1][genePlayer1];
+                    int movePlayer2 = solutions[player2][genePlayer2];
+
+                    //Update history after player1 and player2 both make moves
+                    for (int i=0; i<4; i++)
+                    {
+                        history1[i] = history1[i+2];
+                        history2[i] = history2[i+2];
+                    }
+                    history1[4] = movePlayer1;
+                    history1[5] = movePlayer2;
+                    history2[4] = movePlayer2;
+                    history2[5] = movePlayer1;
 					
 					
 					/*
@@ -348,33 +348,33 @@ public class PrisonersDilemma
 						If both cooperate, +3 for both. If both defect, +1 for both.
 						If one cooperates and one defects, +0 for the one who cooperates and +5 for the one who defects.
 					*/
-					
-					if (movePlayer1==1 && movePlayer2==1) //if both cooperate
-					{
-						score[player1] += 3;
-						score[player2] += 3;	
-					}
-					else if (movePlayer1==0 && movePlayer2==0) //if both defect
-					{
-						score[player1] += 1;
-						score[player2] += 1;	
-					}
-					else if (movePlayer1==1 && movePlayer2==0) //if player1 cooperates and player2 defects
-					{
-						score[player1] += 0;
-						score[player2] += 5;	
-					}
-					else if (movePlayer1==0 && movePlayer2==1) //if player1 defects and player2 cooperates
-					{
-						score[player1] += 5;
-						score[player2] += 0;	
-					}
-				}
-			}
-		}
-	}
-	
-	public static int situation(int[] history) //gives the gene number
+
+                    if (movePlayer1==1 && movePlayer2==1) //if both cooperate
+                    {
+                        score[player1] += 3;
+                        score[player2] += 3;
+                    }
+                    else if (movePlayer1==0 && movePlayer2==0) //if both defect
+                    {
+                        score[player1] += 1;
+                        score[player2] += 1;
+                    }
+                    else if (movePlayer1==1 && movePlayer2==0) //if player1 cooperates and player2 defects
+                    {
+                        score[player1] += 0;
+                        score[player2] += 5;
+                    }
+                    else if (movePlayer1==0 && movePlayer2==1) //if player1 defects and player2 cooperates
+                    {
+                        score[player1] += 5;
+                        score[player2] += 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public static int situation(int[] history) //gives the gene number
     {
 		/*
 			Reminder: 
@@ -382,74 +382,74 @@ public class PrisonersDilemma
 			history[2] == player's second move, history[3] == opponent's second move
 			history[4] == player's third move, history[5] == opponent's third move
 		*/
-		int situation = 0;
+        int situation = 0;
 		
 		/*in determining the first three moves of a round, 
 		   we don't care about the previous moves of the player in question himself, 
 		   we only care about the previous moves of the player's opponent (which are history[3] and history[5])
         */
-		if (history[5] == -1) //first move of the round hasn't been made yet
-			situation = 0;
-		else if (history[3] == -1 && history[5] == 1) //first move of opponent was cooperate
-			situation = 1;
-		else if (history[3] == -1 && history[5] == 0) //first move of opponent was defect
-			situation = 2;
-		else if (history[1] == -1 && history[3] == 1 && history[5] == 1) //opponent's 1st move && 2nd move == cooperate
-			situation = 3;
-		else if (history[1] == -1 && history[3] == 1 && history[5] == 0) //opponent's 1st move == cooperate, 2nd move == defect
-			situation = 4;
-		else if (history[1] == -1 && history[3] == 0 && history[5] == 1) //opponent's 1st move == defect, 2nd move == cooperate
-			situation = 5;
-		else if (history[1] == -1 && history[3] == 0 && history[5] == 0) //opponent's 1st move && 2nd move == defect
-			situation = 6;
+        if (history[5] == -1) //first move of the round hasn't been made yet
+            situation = 0;
+        else if (history[3] == -1 && history[5] == 1) //first move of opponent was cooperate
+            situation = 1;
+        else if (history[3] == -1 && history[5] == 0) //first move of opponent was defect
+            situation = 2;
+        else if (history[1] == -1 && history[3] == 1 && history[5] == 1) //opponent's 1st move && 2nd move == cooperate
+            situation = 3;
+        else if (history[1] == -1 && history[3] == 1 && history[5] == 0) //opponent's 1st move == cooperate, 2nd move == defect
+            situation = 4;
+        else if (history[1] == -1 && history[3] == 0 && history[5] == 1) //opponent's 1st move == defect, 2nd move == cooperate
+            situation = 5;
+        else if (history[1] == -1 && history[3] == 0 && history[5] == 0) //opponent's 1st move && 2nd move == defect
+            situation = 6;
 		
         /*in determining the fourth move up until the last, 
 		  we care about the previous moves of both the player himself and his opponent
         */
-        else 
-		{
-			situation = 7;
-			
-			if (history[0] == 1)
-				situation += 0;
-			else if (history[0] == 0)
-				situation += Math.pow(2,5);
+        else
+        {
+            situation = 7;
 
-			if (history[1] == 1)
-				situation += 0;
-			else if(history[1] == 0)
-				situation += Math.pow(2,4);
+            if (history[0] == 1)
+                situation += 0;
+            else if (history[0] == 0)
+                situation += Math.pow(2,5);
 
-			if (history[2] == 1)
-				situation += 0;
-			else if(history[2] == 0)
-				situation += Math.pow(2,3);
+            if (history[1] == 1)
+                situation += 0;
+            else if(history[1] == 0)
+                situation += Math.pow(2,4);
 
-			if (history[3] == 1)
-				situation += 0;
-			else if(history[3] == 0)
-				situation += Math.pow(2,2);
+            if (history[2] == 1)
+                situation += 0;
+            else if(history[2] == 0)
+                situation += Math.pow(2,3);
 
-			if (history[4] == 1)
-				situation += 0;
-			else if(history[4] == 0)
-				situation += Math.pow(2,1);
-			
-			if (history[5] == 1)
-				situation += 0;
-			else if (history[5] == 0)
-				situation += Math.pow(2,0);
-		}
+            if (history[3] == 1)
+                situation += 0;
+            else if(history[3] == 0)
+                situation += Math.pow(2,2);
+
+            if (history[4] == 1)
+                situation += 0;
+            else if(history[4] == 0)
+                situation += Math.pow(2,1);
+
+            if (history[5] == 1)
+                situation += 0;
+            else if (history[5] == 0)
+                situation += Math.pow(2,0);
+        }
 
         return situation;
     }
-	
-	//The rouletteWheel() method selects next parents based on fitness.
-	public static int[] rouletteWheel(double minFitness)
+
+    //The rouletteWheel() method selects next parents based on fitness.
+    public static int[] rouletteWheel(double minFitness)
     {
-		//set minFitness of the population to the origin (to zero) of the reference frame
-		//add up the fitness of all the population
-		double sum = 0.0;
+        //set minFitness of the population to the origin (to zero) of the reference frame
+        //add up the fitness of all the population
+        double sum = 0.0;
         for (int i = 0; i < population; i++){
             sum += fitness[i] - minFitness;
         }
@@ -464,9 +464,9 @@ public class PrisonersDilemma
         search = 0;
         while (findParent <= luckyNumber && search < fitness.length)
         {
-            findParent += (fitness[search] - minFitness) / sum; 
-			//the bigger the fitness of a player with the index "search", 
-			//the more chance it has of covering the luckyNumber and thus of being selected as parent
+            findParent += (fitness[search] - minFitness) / sum;
+            //the bigger the fitness of a player with the index "search",
+            //the more chance it has of covering the luckyNumber and thus of being selected as parent
             search ++;
         }
 
@@ -487,9 +487,9 @@ public class PrisonersDilemma
         //System.out.println("Parent 2 = " + indices[1]);
 
         return indices;
-	}
-	
-	public static void printChromosome(int[][] array, int index)
+    }
+
+    public static void printChromosome(int[][] array, int index)
     {
         System.out.print("\t");
 
